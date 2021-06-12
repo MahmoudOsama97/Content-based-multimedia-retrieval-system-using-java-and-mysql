e7b011c4a01109d2444f2bf0b9521c7c52f66686
Mahmoud Osama <55949294+MahmoudOsama97@users.noreply.github.com>
2021-06-12 22:31:50 +0300

    final version
    yaaah

--- src/sample/SearchImage.java
+++ src/sample/SearchImage.java
@@ -16,35 +16,30 @@ import java.util.Vector;
 import org.opencv.core.CvType;
 import static org.opencv.core.CvType.*;
 
-
-
-
+//class is used to Search for image by mean color, histogram and color layout
 public class SearchImage {
+    //function used to get the mean value of the image and return it as double value
     static double getMean(Mat image){
-
         double sum=0;
         for(int i=0; i<image.size(0); i++){
             for(int j=0; j<image.size(1);j++ ){
                 sum+=((double)image.get(i,j)[0]);
             }
-
-
         }
         return sum/(image.size(0)*image.size(1));
     }
-
-
+    /*
+    function used to search images in the database by using color layout (grid) takes the string path, the
+    connection to the database and the number of blocks in both vertical and horizontal axis
+     */
     static Vector<String> grid(String args ,Connection conn ,int width,int height) throws SQLException {
         Vector<String> vec = new Vector<String>();
-
-        //String filename = args.length > 0 ? args[0] : "C:\\Users\\osama\\Desktop\\multimedia\\download.jpg";
         String filename = args;
         Mat src = Imgcodecs.imread(filename);
         if (src.empty()) {
             System.err.println("Cannot read image: " + filename);
             System.exit(0);
         }
-
         List<Mat> bgrPlanes = new ArrayList<>();
         Core.split(src, bgrPlanes);
         int size= src.size(0)*src.size(1);
@@ -54,25 +49,17 @@ public class SearchImage {
          List<Mat> tempSrcR = new ArrayList<Mat>();
          List<Mat> tempSrcG = new ArrayList<Mat>();
          List<Mat> tempSrcB = new ArrayList<Mat>();
-
         for( int w =0;w<((src.size(1)/width)*width);w+=wStep){
             for( int h =0;h<((src.size(0)/height)*height);h+=hStep){
-//                System.out.println(bgrPlanes.get(0).size());
-//                //System.out.println(bgrPlanes.get(0).size(0));
-//
-//                System.out.println(w);
-//                System.out.println(h);
                 tempSrcR.add(bgrPlanes.get(0).submat(h,(h+hStep),w,(w+wStep)));
                 tempSrcG.add(bgrPlanes.get(1).submat(h,(h+hStep),w,(w+wStep)));
                 tempSrcB.add(bgrPlanes.get(2).submat(h,(h+hStep),w,(w+wStep)));
             }
         }
-
         String Q="select * from images";
         Statement stmt = conn.createStatement();
         ResultSet R= stmt.executeQuery(Q);
         while(R.next()){
-
             int c=0;
            String modelUrl= R.getString("url");
             Mat model = Imgcodecs.imread(modelUrl);
@@ -80,40 +67,30 @@ public class SearchImage {
                 System.err.println("Cannot read image: " + modelUrl);
                 System.exit(0);
             }
-
             List<Mat> bgrPlanesModel = new ArrayList<>();
             Core.split(model, bgrPlanesModel);
             int sizeM= model.size(0)*model.size(1);
             int number_of_blocksM=sizeM/(width*height);
             int wStepM=(model.size(1)/width);
             int hStepM=(model.size(0)/height);
-            //List<Mat> tempModel= new ArrayList<Mat>();
             Mat tempMR,tempMG,tempMB, tempSR,tempSG,tempSB;
             int counter=0;
             int counter_blocks=0;
             for( int w =0;w<(model.size(1)/width)*width;w+=wStepM){
                 for( int h =0;h<(model.size(0)/height)*height;h+=hStepM){
-
                     tempMR=(bgrPlanesModel.get(0).submat(h,(h+hStepM),w,(w+wStepM)));
                     tempMG=(bgrPlanesModel.get(1).submat(h,(h+hStepM),w,(w+wStepM)));
                     tempMB=(bgrPlanesModel.get(2).submat(h,(h+hStepM),w,(w+wStepM)));
-
                     tempSR=tempSrcR.get(counter);
                     tempSG=tempSrcG.get(counter);
                     tempSB=tempSrcB.get(counter);
                     counter++;
-
                     double meanred  =Math.abs(getMean(tempMR)-getMean(tempSR));
                     double meangreen=Math.abs(getMean(tempMG)-getMean(tempSG));
                     double meanblue =Math.abs(getMean(tempMB)-getMean(tempSB));
-
                     if((meanred<=0.2*getMean(tempSR))&&(meangreen<=0.2*getMean(tempSG))&&(meanblue<=0.2*getMean(tempSB))){
-
                         counter_blocks++;
                     }
-
-
-
                 }
             }
             System.out.println(counter_blocks);
@@ -123,18 +100,15 @@ public class SearchImage {
                     String s = R.getString("url");
                     vec.add(s);
                 }
-//                Mat p = Imgcodecs.imread(s);
-//                HighGui.imshow( " image"+String.valueOf(c), p );
-//                HighGui.waitKey(1000);
                 c++;
-                //break;
             }
-
         }
     return vec;
-
     }
-
+    /*
+    function used to search images in the database by using mean takes the string path, the
+    connection to the database and the number of blocks in both vertical and horizontal axis
+    */
     public static Vector<String> mean(String args, Connection conn) throws SQLException {
 
         System.out.println(args);
@@ -157,59 +131,46 @@ public class SearchImage {
         Imgproc.calcHist(bgrPlanes, new MatOfInt(0), new Mat(), bHist, new MatOfInt(histSize), histRange, accumulate);
         Imgproc.calcHist(bgrPlanes, new MatOfInt(1), new Mat(), gHist, new MatOfInt(histSize), histRange, accumulate);
         Imgproc.calcHist(bgrPlanes, new MatOfInt(2), new Mat(), rHist, new MatOfInt(histSize), histRange, accumulate);
-
-
         int meanR,sumR=0,meanG,sumG=0,meanB,sumB=0;
         for (int i = 0 ; i < 256; i++) {
-
             sumR+= i*((int)rHist.get(i,0)[0]);
             sumG+= i*((int)gHist.get(i,0)[0]);
             sumB+= i*((int)bHist.get(i,0)[0]);
-
         }
         meanR=sumR/(src.size(0)*src.size(1));
         meanG=sumG/(src.size(0)*src.size(1));
         meanB=sumB/(src.size(0)*src.size(1));
-
         Q="select * from images";
         Statement stmt = conn.createStatement();
         ResultSet R= stmt.executeQuery(Q);
-
         while(R.next()){
-
            int meanRD= R.getInt("red_mean");
            int meanGD= R.getInt("green_mean");
            int meanBD= R.getInt("blue_mean");
               if( Math.abs(meanR-meanRD)<=0.1*meanR && Math.abs(meanG-meanGD)<=0.1*meanG && Math.abs(meanB-meanBD)<=0.1*meanB){
-
-                  //System.out.println(R.getString("title"));
                   String title=R.getString("title");
                   if(!title.contains("video")) {
                       String s = R.getString("url");
                       vec.add(s);
                   }
               } else{
-
             }
-
         }
-
         return vec;
     }
 
-
+    /*
+    function used to search images in the database by using Histogram takes the string path, the
+    connection to the database and the number of blocks in both vertical and horizontal axis
+    */
     public  Vector<String> hist(String args ,Connection conn,float compare) throws SQLException {
-
-        //String filename = args.length > 0 ? args[0] : "C:\\Users\\osama\\Desktop\\multimedia\\download.jpg";
         String filename= args ;
         Mat src = Imgcodecs.imread(filename);
-
         if (src.empty()) {
             System.err.println("Cannot read image: " + filename);
             System.exit(0);
         }
         String Q="";
-
         List<Mat> bgrPlanes = new ArrayList<>();
         Core.split(src, bgrPlanes);
         int histSize = 256;
@@ -220,12 +181,9 @@ public class SearchImage {
         Imgproc.calcHist(bgrPlanes, new MatOfInt(0), new Mat(), bHist, new MatOfInt(histSize), histRange, accumulate);
         Imgproc.calcHist(bgrPlanes, new MatOfInt(1), new Mat(), gHist, new MatOfInt(histSize), histRange, accumulate);
         Imgproc.calcHist(bgrPlanes, new MatOfInt(2), new Mat(), rHist, new MatOfInt(histSize), histRange, accumulate);
-
         Mat rHist16 =Mat.zeros(16,1,CV_16UC1);
         Mat gHist16 =Mat.zeros(16,1,CV_16UC1);
         Mat bHist16 =Mat.zeros(16,1,CV_16UC1);
-
-
         for (int i=0;i<16;i++){
             // take  16 consecutive elements from the orginal matrix of 256 element
             Mat temp= rHist.submat( 16*i,16*i+15,0,1 );
@@ -234,7 +192,6 @@ public class SearchImage {
             //insert the sum val in the 16 element mat
             rHist16.put(i,0, sum.val);
             int valueR=(int)sum.val[0];
-
             // take  16 consecutive elements from the orginal matrix of 256 element
             temp= gHist.submat( 16*i,16*i+15,0,1 );
             //sum the 16 element
@@ -242,7 +199,6 @@ public class SearchImage {
             //insert the sum val in the 16 element mat
             gHist16.put(i,0, sum.val);
             int valueG=(int)sum.val[0];
-
             // take  16 consecutive elements from the orginal matrix of 256 element
             temp= bHist.submat( 16*i,16*i+15,0,1 );
             //sum the 16 element
@@ -250,11 +206,7 @@ public class SearchImage {
             //insert the sum val in the 16 element mat
             bHist16.put(i,0, sum.val);
             int valueB=(int)sum.val[0];
-
-
-
             }
-
         Vector<String> vec = new Vector<String>();
         Q="select * from images";
         Statement stmt = conn.createStatement();
@@ -267,15 +219,10 @@ public class SearchImage {
                 tempRM = R.getInt(i + 3);
                 tempGM = R.getInt(i + 19);
                 tempBM = R.getInt(i + 35);
-
-
                 tempRI = ((int) rHist16.get(i, 0)[0]);
                 tempGI = ((int) gHist16.get(i, 0)[0]);
                 tempBI = ((int) bHist16.get(i, 0)[0]);
-
-
                 sum = sum + Math.min(tempRI, tempRM) + Math.min(tempGI, tempGM) + Math.min(tempBI, tempBM);
-                //System.out.println(sum);
             }
             value = sum / (3 * (R.getInt("pixel")));
             if (value > compare) {
@@ -287,13 +234,9 @@ public class SearchImage {
                 }
             }
             sum = 0;
-
         }
-
         System.out.println(counter);
         return vec;
-
     }
-
 }
 
--- src/sample/SearchVideo.java
+++ src/sample/SearchVideo.java
@@ -18,14 +18,12 @@ import java.util.Vector;
 
 import static org.opencv.core.CvType.CV_16UC1;
 
-
+//class is used to Search for videos by Naive Video Similarity
 public class SearchVideo {
-
+    /*
+    function is used to calculate histogram for image and return it
+     */
     public static Mat calcHist(Mat src){
-
-        //System.out.println(src.size());
-
-
         Mat frameGrey = new Mat();
         Imgproc.cvtColor(src,frameGrey,Imgproc.COLOR_RGB2GRAY);
         List<Mat> bgrPlanes = new ArrayList<>();
@@ -39,30 +37,27 @@ public class SearchVideo {
         Scalar S=new Scalar(src.size(0) * src.size(1));
         Core.divide(Hist,S,Hist);
         return Hist ;
-
     }
 
+    /*
+    function is used to search for the videos in the database buy using naive video similarity in which we divide the
+    two sequence into keyframes and find the total number of frames in each sequence then find the number of similar frames
+    divide the number of similar frames over the total number of frames
+     */
     public Vector<String> Search(String args , Connection conn, float compare) throws SQLException {
         Vector<String> vec = new Vector<String>();
         int x=0;
         String Q = "";
         Mat frame = new Mat();
         VideoCapture camera = new VideoCapture(args);
-
         Mat histold = Mat.zeros(256, 1, 5);
-
         Mat frameGrey = new Mat();
         int count = 0;
-
         double k1 = 0.1, k2 = 0.8;
-
         List<Mat> keyFrames = new ArrayList<>();
-
         while (true) {
             if (camera.read(frame)) {
-
                 Imgproc.cvtColor(frame, frameGrey, Imgproc.COLOR_RGB2GRAY);
-
                 List<Mat> bgrPlanes = new ArrayList<>();
                 Core.split(frameGrey, bgrPlanes);
                 int histSize = 256;
@@ -71,108 +66,68 @@ public class SearchVideo {
                 boolean accumulate = false;
                 Mat Hist = new Mat();
                 Imgproc.calcHist(bgrPlanes, new MatOfInt(0), new Mat(), Hist, new MatOfInt(histSize), histRange, accumulate);
-
                 Mat diff = Mat.zeros(256, 1, 5);
-
                 Mat diffSquare = Mat.zeros(256, 1, 5);
-
                 Core.pow(diff, 2, diffSquare);
                 Scalar sumSquare = Core.sumElems(diffSquare);
                 double meanSquare = ((double) sumSquare.val[0]) / 256;
-
                 Core.absdiff(Hist, histold, diff);
                 Scalar sum = Core.sumElems(diff);
                 double mean = ((double) sum.val[0]) / 256;
                 double sd = Math.sqrt(Math.abs(meanSquare - mean));
                 double threshold = k1 * mean + k2 * sd;
-
                 histold = Hist;
-
                 if (threshold > 15) {
                     Imgcodecs.imwrite("C:\\Users\\osama\\Desktop\\multimedia\\test"+String.valueOf(x)+".jpg",frame);
                     x++;
-//                    HighGui.imshow("frame", frame);
-//                    HighGui.waitKey(100);
                     count++;
                 }
-
             }
             else {
                 for(int i=0;i<x;i++) {
                     Mat src = Imgcodecs.imread("C:\\Users\\osama\\Desktop\\multimedia\\test"+String.valueOf(i)+".jpg");
-//                    HighGui.imshow("frame", src);
-
                     keyFrames.add(src);
-                    //System.out.println(keyFrames.get(i).size());
-                    //System.out.println(keyFrames.size());
-
                 }
                 break;
-
             }
-
         }
         int Count2=0;
-
-
         Q="select * from videos";
         Statement stmt = conn.createStatement();
         ResultSet R= stmt.executeQuery(Q);
-        //System.out.println(Q);
         while(R.next()){
             int videoId= R.getInt("id");
             Q="select * from images where videoId = "+String.valueOf(videoId);
             //System.out.println(Q);
             Statement stmt2 = conn.createStatement();
             ResultSet R2= stmt2.executeQuery(Q);
-
             List<Mat> QueryKeyFrames = new ArrayList<>();
-
             while(R2.next()){
                 String frameUrl= R2.getString("url");
-
-                //System.out.println(frameUrl);
                 Mat qFrame = Imgcodecs.imread(frameUrl);
-
                 QueryKeyFrames.add(qFrame);
             }
-
-            //System.out.println(keyFrames.size());
-            //System.out.println(QueryKeyFrames.size());
             for (Mat keyFrameI : keyFrames)
             {
                 for (Mat keyFrameQ : QueryKeyFrames)
                 {
                     Mat Histi= calcHist(keyFrameQ);
-                    //System.out.println(keyFrameI.size());
                     Mat Histq= calcHist(keyFrameI);
-
-
-                   Mat diff = Mat.zeros(256, 1, 5);
-                   Core.absdiff(Histi,Histq,diff);
-                   Scalar sum=Core.sumElems(diff);
-                    //System.out.println(sum.val[0]);
-                   double mean=(((double)sum.val[0]))/256;
-                    //System.out.println("mean"+String.valueOf(mean));
-                   if(mean<=0.002){
-
+                    Mat diff = Mat.zeros(256, 1, 5);
+                    Core.absdiff(Histi,Histq,diff);
+                    Scalar sum=Core.sumElems(diff);
+                    double mean=(((double)sum.val[0]))/256;
+                    if(mean<=0.002){
                        Count2++;
-
                    }
-
                 }
-
             }
-            //if((Count2*1.0/(QueryKeyFrames.size()+keyFrames.size()))>=compare*10){
             if((Count2*1.0/(QueryKeyFrames.size()))>=compare){
-
                 String s = R.getString("url");
                 vec.add(s);
             }
             Count2=0;
         }
-
-
     return vec;
 
     }
