58a3a2e5eb88bb643dfca65382dad1832014c3ef
Swilam20 <38882447+Swilam20@users.noreply.github.com>
2021-06-12 23:18:23 +0200

    Update InsertImage.java

--- src/InsertImage.java
+++ src/InsertImage.java
@@ -143,30 +143,6 @@ class InsertImage {
 
 
 
-//        int histW = 512, histH = 400;
-//        int binW = (int) Math.round((double) histW / histSize);
-//        Mat histImage = new Mat( histH, histW, CV_8UC3, new Scalar( 0,0,0) );
-//        Core.normalize(bHist, bHist, 0, histImage.rows(), Core.NORM_MINMAX);
-//        Core.normalize(gHist, gHist, 0, histImage.rows(), Core.NORM_MINMAX);
-//        Core.normalize(rHist, rHist, 0, histImage.rows(), Core.NORM_MINMAX);
-//        float[] bHistData = new float[(int) (bHist.total() * bHist.channels())];
-//        bHist.get(0, 0, bHistData);
-//        float[] gHistData = new float[(int) (gHist.total() * gHist.channels())];
-//        gHist.get(0, 0, gHistData);
-//        float[] rHistData = new float[(int) (rHist.total() * rHist.channels())];
-//        rHist.get(0, 0, rHistData);
-//
-//        for( int i = 1; i < histSize; i++ ) {
-//            Imgproc.line(histImage, new Point(binW * (i - 1), histH - Math.round(bHistData[i - 1])),
-//                    new Point(binW * (i), histH - Math.round(bHistData[i])), new Scalar(255, 0, 0), 2);
-//            Imgproc.line(histImage, new Point(binW * (i - 1), histH - Math.round(gHistData[i - 1])),
-//                    new Point(binW * (i), histH - Math.round(gHistData[i])), new Scalar(0, 255, 0), 2);
-//            Imgproc.line(histImage, new Point(binW * (i - 1), histH - Math.round(rHistData[i - 1])),
-//                    new Point(binW * (i), histH - Math.round(rHistData[i])), new Scalar(0, 0, 255), 2);
-//        }
-//        HighGui.imshow( "Source image", src );
-//        HighGui.imshow( "calcHist Demo", histImage );
-//        HighGui.waitKey(0);
-//        System.exit(0);
+
     }
 }
